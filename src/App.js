63b4deae45c51417664b185256254000140717fb
Gabriel Nicolosi <gabianp@live.com.ar>
2023-02-26 08:56:26 -0300

    Agrego Footer

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import Cart from './pages/Cart/Cart';
 import ItemDetailContainer from './pages/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './pages/ItemListContainer/ItemListContainer';
 import CartProvider from './context/CartProvider';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
-    <BrowserRouter>
-      <CartProvider>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<ItemListContainer />} />
-          <Route path="/category/:categoryId" element={<ItemListContainer />} />
-          <Route path="/item/:id" element={<ItemDetailContainer />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </CartProvider>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <div className="content-wrap">
+          <CartProvider>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<ItemListContainer />} />
+              <Route
+                path="/category/:categoryId"
+                element={<ItemListContainer />}
+              />
+              <Route path="/item/:id" element={<ItemDetailContainer />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </CartProvider>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 export default App;
